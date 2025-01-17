7a844d2917ed73a261307314d3c5c33661deac1f
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-17 23:12:51 +0600

    Update ProductItem.jsx

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const ProductItem = ({id,image,name,price}) => {
 
-        const {currency} = useContext(ShopContext)
+  const {currency} = useContext(ShopContext)
 
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
